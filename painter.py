dfac2d40741d99e4b293edb8d8c3ed3587477c0a
EkhlasMridhaShimul <ekhlas29@student.sust.edu>
2020-07-03 08:55:14 +0600

    brush config

--- painter.py
+++ painter.py
@@ -5,13 +5,18 @@ root.title("Painter")
 root.geometry("800x600")
 
 def paint(e):
+    brushSize=20
+    brushColor="blue"
+    #brush types: ROUND, ROUND, PROJECTING
+    brushType=PROJECTING
+    
     x1=e.x -1
     y1=e.y -1
     
     x2=e.x +1
     y2 = e.y+1
     
-    canvas.create_line(x1,y1,x2,y2,fill="green")
+    canvas.create_line(x1,y1,x2,y2,fill=brushColor,width=brushSize,capstyle=brushType,smooth=True)
 
 w=600
 h=400
